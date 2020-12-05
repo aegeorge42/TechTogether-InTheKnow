3fecad9fec4c32576bdbb5df8aa5aa55dac52818
noemiems <59896235+noemiems@users.noreply.github.com>
2020-12-05 11:56:21 -0600

    Update keywordsfinder.py

--- keywordsfinder.py
+++ keywordsfinder.py
@@ -12,7 +12,8 @@ nlp = spacy.load("en_core_web_lg")
 
 def get_keywords(text):
     result = []
-    pos_tag = ['PROPN', 'ADJ', 'NOUN']
+    #pos_tag = ['PROPN', 'ADJ', 'NOUN']
+    pos_tag = ['PROPN', 'NOUN']
     document = nlp(text.lower())
     for token in document:
         if(token.text in nlp.Defaults.stop_words or token.text in punctuation):
@@ -25,9 +26,13 @@ def get_keywords(text):
 keywordsteacher = set(get_keywords(teacher))
 keywordsbad = set(get_keywords(bad))
 
+cnt = 0
 for item in keywordsteacher:
     if item in keywordsbad:
-        print(item)
+        cnt += 1
 
-doc = nlp(teacher)
-print([(X.text, X.label_) for X in doc.ents])
+lenkeywordsteacher = len(keywordsteacher)
+keywordscore = int((cnt*100)/lenkeywordsteacher)
+print(f"Similarity score = {keywordscore}%")
+#doc = nlp(teacher)
+#print([(X.text, X.label_) for X in doc.ents])
