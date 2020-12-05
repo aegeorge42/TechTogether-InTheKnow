3c7be93dbdc0bb91067d968946480a06e0179b72
noemiems <59896235+noemiems@users.noreply.github.com>
2020-12-05 11:46:05 -0600

    Update keywordsfinder.py

--- keywordsfinder.py
+++ keywordsfinder.py
@@ -1,9 +1,11 @@
 import spacy
+from spacy import displacy
+from collections import Counter
 from string import punctuation
 
-TEACHERNOTES = open("shortnotes.txt")
+GOODNOTES = open("shortnotes.txt")
 BADNOTES = open("badnotes.txt")
-teacher = TEACHERNOTES.read()
+good = GOODNOTES.read()
 bad = BADNOTES.read()
 
 nlp = spacy.load("en_core_web_lg")
@@ -11,8 +13,8 @@ nlp = spacy.load("en_core_web_lg")
 def get_keywords(text):
     result = []
     pos_tag = ['PROPN', 'ADJ', 'NOUN']
-    doc = nlp(text.lower())
-    for token in doc:
+    document = nlp(text.lower())
+    for token in document:
         if(token.text in nlp.Defaults.stop_words or token.text in punctuation):
             continue
         if(token.pos_ in pos_tag):
@@ -20,9 +22,12 @@ def get_keywords(text):
     return result
 
 
-keywordsteacher = set(get_keywords(teacher))
+keywordsgood = set(get_keywords(good))
 keywordsbad = set(get_keywords(bad))
 
-for item in keywordsteacher:
+for item in keywordsgood:
     if item in keywordsbad:
         print(item)
+
+doc = nlp(good)
+print([(X.text, X.label_) for X in doc.ents])
