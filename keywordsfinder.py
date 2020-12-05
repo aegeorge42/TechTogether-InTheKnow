13d2bf18278cd9d62e9d107da4aa5a6480762af5
Michelle Watson <alicerockshard@gmail.com>
2020-12-05 18:59:28 -0500

    changed list variable due to python keyword conflict. Print out teacher keywords

--- keywordsfinder.py
+++ keywordsfinder.py
@@ -13,16 +13,16 @@ nlp = en_core_web_lg.load()
 kwteacher = []
 kwstudent = []
 
-def extract_keywords(text, list):
+def extract_keywords(text, extractedList):
     extractor = pke.unsupervised.TopicRank()
     extractor.load_document(input=text, language='en')
     extractor.candidate_selection()
     extractor.candidate_weighting()
     keyphrases = extractor.get_n_best(n=20)
     for item in range(len(keyphrases)):
-        list.append(keyphrases[item][0])
+        extractedList.append(keyphrases[item][0])
 
-    return list
+    return extractedList
 
 extract_keywords("data/neurosci_wiki_people_history.txt", kwteacher)
 extract_keywords("data/neurosci_bad_notes.txt", kwstudent)
@@ -51,4 +51,5 @@ print(tagkwdict)
 lenkwteacher = len(kwteacher)
 kwscore = int((cnt*100)/lenkwteacher)
 print(f"Similarity score = {kwscore}%")
+print("Teacher's study terms: ", kwteacher)
 print("Suggested study terms: ", suggestedStudyTerms)
